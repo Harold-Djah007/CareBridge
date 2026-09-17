86ab84b15aad3278c602eaa859ad1181a480a1d0
Harold-Djah007 <146589784+Harold-Djah007@users.noreply.github.com>
2026-09-17 11:32:01 +0000

    Load legacy Midnight bridge after core integrity styles

--- CareBridge_Health_Consultation_FULL_PROJECT/CareBridge_Health_Consultation/client/src/main.jsx
+++ CareBridge_Health_Consultation_FULL_PROJECT/CareBridge_Health_Consultation/client/src/main.jsx
@@ -29,6 +29,7 @@ import "./carebridge-patient-experience.css";
 import "./carebridge-clinical-orders.css";
 import "./carebridge-quality-gate.css";
 import "./carebridge-theme-integrity.css";
+import "./carebridge-theme-integrity-legacy.css";
 import { homeFor } from "./utils";
 import { AuthProvider, ToastProvider, useAuth } from "./state";
 import { PatientExperienceProvider, usePatientExperience } from "./patientExperience";
