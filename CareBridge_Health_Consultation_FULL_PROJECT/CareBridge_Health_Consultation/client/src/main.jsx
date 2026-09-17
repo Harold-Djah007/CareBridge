149cca47019e6df8e0be22a3ecf36edd2f342bb0
Harold-Djah007 <146589784+Harold-Djah007@users.noreply.github.com>
2026-09-17 11:28:57 +0000

    Load theme integrity layer after all feature styles

--- CareBridge_Health_Consultation_FULL_PROJECT/CareBridge_Health_Consultation/client/src/main.jsx
+++ CareBridge_Health_Consultation_FULL_PROJECT/CareBridge_Health_Consultation/client/src/main.jsx
@@ -28,6 +28,7 @@ import "./carebridge-notification-portal.css";
 import "./carebridge-patient-experience.css";
 import "./carebridge-clinical-orders.css";
 import "./carebridge-quality-gate.css";
+import "./carebridge-theme-integrity.css";
 import { homeFor } from "./utils";
 import { AuthProvider, ToastProvider, useAuth } from "./state";
 import { PatientExperienceProvider, usePatientExperience } from "./patientExperience";
